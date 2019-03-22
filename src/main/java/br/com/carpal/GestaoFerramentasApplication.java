0a2d1b62b30c9ebc4b69dd9f979f983698c8825e
Dowglas Maia <dowglasmaia@gmail.com>
2019-03-22 09:28:52 -0300

    Impl-> Conexão com MySQL

--- src/main/java/br/com/carpal/GestaoFerramentasApplication.java
+++ src/main/java/br/com/carpal/GestaoFerramentasApplication.java
@@ -1,20 +1,24 @@
 package br.com.carpal;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestaoFerramentasApplication implements CommandLineRunner{
+public class GestaoFerramentasApplication implements CommandLineRunner {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoFerramentasApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+
 	}
 
 }
---
+++ src/main/java/br/com/carpal/services/CargoService.java
@@ -0,0 +1,8 @@
+package br.com.carpal.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CargoService {
+
+}
